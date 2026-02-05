7e2b7497fdd74ad46480d158900c6a6dae074847
pubwordncode <pub@wordncode.com>
2026-02-05 18:00:18 +0900

    test

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 "use strict";
-var 장바구니 = {
-    plus: function (x, y) { return x + y; }
-};
-console.log(장바구니.plus(3, 6));
+var Square = /** @class */ (function () {
+    function Square(width, height, bg) {
+        this.width = width;
+        this.height = height;
+        this.bg = bg;
+    }
+    Square.prototype.draw = function () {
+        var _a;
+        var body = document.querySelector('body');
+        var box = document.createElement('div');
+        var randomX = Math.floor(Math.random() * (window.innerWidth));
+        var randomY = Math.floor(Math.random() * (window.innerHeight));
+        box.style.width = "".concat(this.width, "px");
+        box.style.height = "".concat(this.height, "px");
+        box.style.background = this.bg;
+        box.style.position = 'absolute';
+        box.style.left = "".concat(randomX, "px");
+        box.style.top = "".concat(randomY, "px");
+        (_a = document.body) === null || _a === void 0 ? void 0 : _a.appendChild(box);
+    };
+    return Square;
+}());
+var 네모 = new Square(30, 30, 'red');
+네모.draw();
+네모.draw();
--- index.ts
+++ index.ts
@@ -1,7 +1,34 @@
-interface Child {
-  plus: (x: number, y: number) => number;
+class Square{
+    private width: number
+    private height: number
+    private bg: string
+    constructor(width:number,height:number,bg:string){
+        this.width = width
+        this.height = height
+        this.bg = bg
+    }
+
+    public draw(){
+        const body = document.querySelector('body')
+        const box = document.createElement('div')
+
+        const randomX = Math.floor(Math.random() * (window.innerWidth));
+        const randomY = Math.floor(Math.random() * (window.innerHeight));
+
+
+        box.style.width = `${this.width}px`;
+        box.style.height = `${this.height}px`;
+        box.style.background = this.bg;
+
+
+        box.style.position = 'absolute'; 
+        box.style.left = `${randomX}px`;
+        box.style.top = `${randomY}px`;
+
+
+        document.body?.appendChild(box);
+    }
 }
-const 장바구니: Child = {
-  plus :(x,y) => x + y
-};
-console.log(장바구니.plus(3,6));
+let 네모 = new Square(30, 30, 'red');
+네모.draw()
+네모.draw()
